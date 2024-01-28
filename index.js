906bf8afa01c73ccc4e4efe1445b46f4cc885e18
Yukie M Billal <yukiembillal01@gmail.com>
2024-01-28 21:57:37 +0700

    feat: finish transfer with remote

--- index.js
+++ index.js
@@ -2,3 +2,5 @@ console.log('HELLO GITHUB!')
 console.log('Learn vim!')
 
 console.log('exist repo transfered to https://github.com/GMNocK')
+
+console.log('working for the commit, but no for issue & PR log')
